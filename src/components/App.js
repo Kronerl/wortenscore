a6ad7af0815eb2892310fdc8f6672df9681a7213
Krone <67793556+Kronerl@users.noreply.github.com>
2023-05-21 15:21:47 +0100

    Layout edit

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react'
 import Logored from '../imagens/Worten-red.png';
 import Logoblue from '../imagens/Worten-blue.png';
+import LogoX from '../imagens/X.png';
 
 
 
@@ -22,27 +23,97 @@ function App() {
         <div className="center-side">
 
           <div className='titlediv'>
-            <p className='title'> Worten Casa </p>
+            {/* <p className='title'> Worten Casa </p> */}
           </div>
           <div className='scoreboarddiv'>
 
             <div className='scoreboard'>
 
-
-              <button className='count' onClick={() => setCount((count) => count + 1)}>
+              {/* <button className='count' onClick={() => setCount((count) => count + 1)}>
                   {count}
-              </button>
-              <div>
+              </button> */}
+              
 
-                  <p className='title'> Worten Tech</p>
 
-              </div>
+                  <div className='score-wortencasa'>
 
-              <div>
+                    <p className='title'> Worten Casa </p>
 
-                  <p className='title'> Garrantia</p>
+                  </div>
+                  <div className='scores'>
+                    <div className='score-section'>
 
-              </div>
+                      <p className='score-font'> 5 </p>
+
+                    </div>
+                    <div className='score-section'>
+
+                      <img className='LogoX' src={LogoX} />
+
+                    </div>
+                    <div className='score-section'>
+
+                      <p className='score-font'> 2 </p>
+
+                    </div>
+                  </div>
+
+
+
+
+              
+                <div className='score-wortentech'>
+
+                  <p className='title'>Worten Tech</p>
+
+                </div>
+                <div className='scores'>
+                  <div className='score-section'>
+
+                    <p className='score-font'> 9 </p>
+
+                  </div>
+                  <div className='score-section'>
+
+                    <img className='LogoX' src={LogoX} />
+
+                  </div>
+                  <div className='score-section'>
+
+                    <p className='score-font'> 8 </p>
+
+                  </div>
+                </div>
+
+
+
+
+
+
+                <div className='score-wortenge'>
+
+                  <p className='title'> Norton </p>
+
+                </div>
+                <div className='scores'>
+                  <div className='score-section'>
+
+                    <p className='score-font'> 5 </p>
+
+                  </div>
+                  <div className='score-section'>
+
+                    <img className='LogoX' src={LogoX} />
+
+                  </div>
+                  <div className='score-section'>
+
+                    <p className='score-font'> 2 </p>
+
+                  </div>
+                </div>
+
+
 
 
 
